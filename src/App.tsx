0c1743ffeb2dc42712bccc25898f079642c8eb3c
杨金豪 <1536845096@qq.com>
2022-11-16 22:18:25 +0800

    ‘在原有基础上加入Day.js’

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,15 @@ import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Detail from './Detail';
 import Lists from './Lists';
+import Login from './Login';
 import Main from './Main'
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Main/>} />
+          <Route path="/" element={<Main />} />
+          <Route path="/Login" element={<Login/>} />
           <Route path="/Detail/:username/:name" element={<Detail/>} />
         </Routes>
       </BrowserRouter>
---
+++ src/Login.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Input } from 'antd';
+const Login = () => {
+  return (
+    <>
+      <div className="main">
+        <Input placeholder="Basic usage" />
+      </div>
+    </>
+    
+  )
+}
+
+export default Login
